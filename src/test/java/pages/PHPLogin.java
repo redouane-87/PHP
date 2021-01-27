2ed52a60efb3b6d4eca94d7a6379abe7458ccadb
redou <redouanemessaoudi87@gmail.com>
2021-01-27 13:08:27 -0500

    start1

--- src/test/java/pages/PHPLogin.java
+++ src/test/java/pages/PHPLogin.java
@@ -1,5 +1,33 @@
 package pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class PHPLogin {
+    public WebDriver driver;
 
+    //class constructor
+
+    public PHPLogin(WebDriver driver){
+
+        this.driver=driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(linkText = "Featured Hotels")
+    List<WebElement> hotels;
+
+    public int MakeReservation(){
+
+         return hotels.size();
+
+
+
+
+
+    }
 }
+
--- src/test/java/steps/PHPLoginTest.java
+++ src/test/java/steps/PHPLoginTest.java
@@ -1,5 +1,49 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.PHPLogin;
+
+import java.util.concurrent.TimeUnit;
+
 public class PHPLoginTest {
+    public static WebDriver driver;
+    String URL= "https://www.phptravels.net/";
+    PHPLogin phpLogin;
+
+
+    @Given("user navigate to URL")
+    public void user_navigate_to_url() {
+        System.setProperty("webdriver.chrome.driver","c:\\Drivers\\chromedriver.exe\\");
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get(URL);
+
+
+
+    }
+
+    @And("user should see PHP travels home page")
+    public void user_should_see_php_travels_home_page() {
+        phpLogin= new PHPLogin(driver);
+
+
+    }
+
+    @Then("user make a reservation")
+    public void user_make_a_reservation() {
+        phpLogin= new PHPLogin(driver);
+        phpLogin.MakeReservation();
+
+
+
+
+    }
+
+
 
 }
